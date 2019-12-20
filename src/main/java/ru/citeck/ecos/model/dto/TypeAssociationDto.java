b4899da192011a4f251527fc2b84c76c3874efe1
olegraskin <oraskin>
2019-12-20 18:20:05 +0400

    ECOSENT-1177 - Add associations inheritance for ecos types (With removing ext_id's constraint)

--- src/main/java/ru/citeck/ecos/model/dto/TypeAssociationDto.java
+++ src/main/java/ru/citeck/ecos/model/dto/TypeAssociationDto.java
@@ -1,8 +1,12 @@
 package ru.citeck.ecos.model.dto;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import ru.citeck.ecos.apps.app.module.type.model.type.AssocDirection;
 import ru.citeck.ecos.records2.RecordRef;
+
+import java.util.Objects;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -13,4 +17,17 @@ public class TypeAssociationDto {
     private String name;
     private RecordRef targetType;
     private AssocDirection direction;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        TypeAssociationDto that = (TypeAssociationDto) o;
+        return id.equals(that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
