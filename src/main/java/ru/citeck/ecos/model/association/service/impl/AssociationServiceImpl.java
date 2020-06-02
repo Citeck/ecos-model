cb5fb82ee53d4542183a40dbe08e3a361632c67e
Oleg Raskin <raskin.o@yandex.ru>
2020-06-02 16:11:11 +0400

    ECOSCOM-3644 - Association can't be removed from type Code reformatting

--- src/main/java/ru/citeck/ecos/model/association/service/impl/AssociationServiceImpl.java
+++ src/main/java/ru/citeck/ecos/model/association/service/impl/AssociationServiceImpl.java
@@ -1,18 +1,11 @@
 package ru.citeck.ecos.model.association.service.impl;
 
 import lombok.RequiredArgsConstructor;
-import org.apache.commons.lang3.StringUtils;
 import org.apache.logging.log4j.util.Strings;
 import org.springframework.stereotype.Service;
-import ru.citeck.ecos.model.converter.DtoConverter;
 import ru.citeck.ecos.model.association.domain.AssociationEntity;
-import ru.citeck.ecos.model.type.domain.TypeEntity;
-import ru.citeck.ecos.model.association.dto.AssociationDto;
 import ru.citeck.ecos.model.association.repository.AssociationRepository;
-import ru.citeck.ecos.model.type.dto.TypeWithMetaDto;
-import ru.citeck.ecos.model.type.repository.TypeRepository;
 import ru.citeck.ecos.model.association.service.AssociationService;
-import ru.citeck.ecos.records2.RecordRef;
 
 import java.util.Set;
 import java.util.UUID;
