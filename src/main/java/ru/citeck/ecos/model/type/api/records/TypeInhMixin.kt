509540f8eef8bba65b15f14364fb0d10fef18357
Zaytsev Aleksey <aleksey.zaytsev@citeck.ru>
2021-03-03 11:08:07 +0700

    ECOSCOM-4209 - Additional Documents. Business-process doesn't start PR fix

--- src/main/java/ru/citeck/ecos/model/type/api/records/TypeInhMixin.kt
+++ src/main/java/ru/citeck/ecos/model/type/api/records/TypeInhMixin.kt
@@ -17,7 +17,6 @@ final class TypeInhMixin(
             "extId",
             "parent",
             RecordConstants.ATT_PARENT,
-            "children",
             RecordConstants.ATT_ACTIONS,
             "assocsFull",
             "form",
@@ -118,9 +117,6 @@ final class TypeInhMixin(
             }
             "resolvedDocLib" -> {
                 rtypeDef.getDocLib()
-            }
-            "children" -> {
-                rtypeDef.getChildren()
             }
             else -> {
                 null
