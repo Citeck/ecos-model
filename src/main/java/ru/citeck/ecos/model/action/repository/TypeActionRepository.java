10542bfb9f6fc9c02b3e64aaa797de298296483c
Pavel Simonov <pavel.simonov@citeck.ru>
2021-05-04 16:14:50 +0700

    Clean actions and associations

--- src/main/java/ru/citeck/ecos/model/action/repository/TypeActionRepository.java
+++
@@ -1,7 +0,0 @@
-package ru.citeck.ecos.model.action.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.citeck.ecos.model.action.domain.TypeActionEntity;
-
-public interface TypeActionRepository extends JpaRepository<TypeActionEntity, Long> {
-}
