c34e4e60d8d75ebd83a75fd5fe66527038404d29
Roman Makarskiy <romanchabest55@gmail.com>
2023-05-11 16:03:46 +0300

    formatting

--- src/test/java/ru/citeck/ecos/model/validator/CommentValidatorTest.java
+++ src/test/java/ru/citeck/ecos/model/validator/CommentValidatorTest.java
@@ -11,53 +11,54 @@ public class CommentValidatorTest {
 
 
     @Test
-    public void shouldCleanInlineCSS(){
+    public void shouldCleanInlineCSS() {
         String result = CommentValidator.removeVulnerabilities("<h2>Inline CSS Example</h2>\n" +
-                        "  <p><span style=\"color: #FF7A59\">inline</span> Test message </p>");
+            "  <p><span style=\"color: #FF7A59\">inline</span> Test message </p>");
+
         Assertions.assertFalse(result.contains("style"));
-
     }
 
     @Test
-    public void shouldCleanInternalCSS(){
+    public void shouldCleanInternalCSS() {
         String result = CommentValidator.removeVulnerabilities("<!DOCTYPE html>\n" +
-                        "<html>\n" +
-                        "    <head>\n" +
-                        "        <style>\n" +
-                        "            p {\n" +
-                        "                color: #FF7A59;\n" +
-                        "            }\n" +
-                        "        </style>\n" +
-                        "    </head>\n" +
-                        "    <body>\n" +
-                        "        <h2>Text message</h2>\n" +
-                        "    </body>\n" +
-                        "</html>");
-                Assertions.assertFalse(result.contains("<style>"));
+            "<html>\n" +
+            "    <head>\n" +
+            "        <style>\n" +
+            "            p {\n" +
+            "                color: #FF7A59;\n" +
+            "            }\n" +
+            "        </style>\n" +
+            "    </head>\n" +
+            "    <body>\n" +
+            "        <h2>Text message</h2>\n" +
+            "    </body>\n" +
+            "</html>");
 
+        Assertions.assertFalse(result.contains("<style>"));
     }
 
     @Test
-    public void shouldRemoveHref(){
+    public void shouldRemoveHref() {
         String result = CommentValidator.removeVulnerabilities("<link rel=\"stylesheet\" type=\"text/css\" rel=\"noopener\" target=\"_blank\" href=\"mystyles.css\">");
+
         Assertions.assertFalse(result.contains("href"));
-
     }
 
     @Test
-    public void shouldRemoveScript(){
+    public void shouldRemoveScript() {
         String result = CommentValidator.removeVulnerabilities("<script>\n" +
-                "document.getElementById(\"demo\").innerHTML = \"Hello JavaScript!\";\n" +
-                "</script>");
-        Assertions.assertFalse(result.contains("<script>"));
+            "document.getElementById(\"demo\").innerHTML = \"Hello JavaScript!\";\n" +
+            "</script>");
 
+        Assertions.assertFalse(result.contains("<script>"));
     }
 
     @Test
-    public void shouldNotRemoveRequiredTags(){
+    public void shouldNotRemoveRequiredTags() {
         String result = CommentValidator.removeVulnerabilities("<p><strong>strong test </strong>" +
             "<em>test message</em> <u>underscore test</u></p>" +
             "<ul>  <li>list item</li>  <li>list item</li></ul>");
+
         Assertions.assertTrue(result.contains("<strong>") &&
             result.contains("<em>") &&
             result.contains("<u>") &&
