94a932929dc005a3122feecb03b34a3ab2ba47aa
Pavel Simonov <pavel.simonov@citeck.ru>
2023-07-12 11:16:47 +0700

    ECOSENT-2515 - Migrate credentials logic to ecos-model

--- src/main/java/ru/citeck/ecos/model/domain/endpoint/eapp/EndpointArtifactHandler.kt
+++ src/main/java/ru/citeck/ecos/model/domain/endpoint/eapp/EndpointArtifactHandler.kt
@@ -6,15 +6,22 @@ import ru.citeck.ecos.commons.data.MLText
 import ru.citeck.ecos.data.sql.records.DbRecordsDao
 import ru.citeck.ecos.data.sql.records.listener.DbRecordChangedEvent
 import ru.citeck.ecos.data.sql.records.listener.DbRecordsListenerAdapter
+import ru.citeck.ecos.events2.EventsService
+import ru.citeck.ecos.events2.type.RecordChangedEvent
+import ru.citeck.ecos.model.lib.type.constants.TypeConstants
+import ru.citeck.ecos.model.lib.utils.ModelUtils
+import ru.citeck.ecos.records2.RecordConstants
+import ru.citeck.ecos.records2.predicate.model.Predicates
 import ru.citeck.ecos.records3.RecordsService
+import ru.citeck.ecos.records3.record.atts.schema.annotation.AttName
 import ru.citeck.ecos.webapp.api.EcosWebAppApi
 import ru.citeck.ecos.webapp.api.entity.EntityRef
 import java.util.function.Consumer
 
 @Component
 class EndpointArtifactHandler(
-    private val webAppApi: EcosWebAppApi,
-    private val recordsService: RecordsService
+    private val recordsService: RecordsService,
+    private val eventsService: EventsService
 ) : EcosArtifactHandler<EndpointArtifactHandler.EndpointDto> {
 
     companion object {
@@ -34,14 +41,16 @@ class EndpointArtifactHandler(
     }
 
     override fun listenChanges(listener: Consumer<EndpointDto>) {
-        webAppApi.doBeforeAppReady {
-            val endpointDao = recordsService.getRecordsDao(ENDPOINT_SRC_ID, DbRecordsDao::class.java)
-                ?: error("Records DAO doesn't found by ID '$ENDPOINT_SRC_ID'")
-            endpointDao.addListener(object : DbRecordsListenerAdapter() {
-                override fun onChanged(event: DbRecordChangedEvent) {
-                    listener.accept(recordsService.getAtts(event.record, EndpointDto::class.java))
-                }
-            })
+        eventsService.addListener<EndpointChangedEventAtts> {
+            withDataClass(EndpointChangedEventAtts::class.java)
+            withEventType(RecordChangedEvent.TYPE)
+            withLocal(true)
+            withFilter(Predicates.eq(
+                "record._type.${TypeConstants.ATT_IS_SUBTYPE_OF}.endpoint?bool", true)
+            )
+            withAction {
+                listener.accept(EndpointDto(it.id, it.name, it.url, it.credentials))
+            }
         }
     }
 
@@ -49,6 +58,17 @@ class EndpointArtifactHandler(
         val id: String,
         val name: MLText?,
         val url: String,
+        val credentials: EntityRef?
+    )
+
+    private class EndpointChangedEventAtts(
+        @AttName("record?localId")
+        val id: String,
+        @AttName("record.name?json")
+        val name: MLText?,
+        @AttName("record.url?str")
+        val url: String,
+        @AttName("record.credentials?id")
         val credentials: EntityRef?
     )
 }
