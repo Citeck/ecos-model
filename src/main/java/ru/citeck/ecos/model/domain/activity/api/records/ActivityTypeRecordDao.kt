fdec22b034da035c9470b200dfc280ea9ab66ef6
Pavel Simonov <pavel.simonov@citeck.ru>
2024-08-01 12:40:46 +0700

    ECOSENT-3033 - CRM: activites widget

--- src/main/java/ru/citeck/ecos/model/domain/activity/api/records/ActivityTypeRecordDao.kt
+++ src/main/java/ru/citeck/ecos/model/domain/activity/api/records/ActivityTypeRecordDao.kt
@@ -1,21 +1,20 @@
 package ru.citeck.ecos.model.domain.activity.api.records
 
 import org.springframework.stereotype.Component
-import ru.citeck.ecos.context.lib.auth.AuthContext
+import ru.citeck.ecos.model.lib.type.service.TypeRefService
 import ru.citeck.ecos.model.lib.utils.ModelUtils
-import ru.citeck.ecos.records2.predicate.PredicateService
-import ru.citeck.ecos.records2.predicate.model.Predicates
 import ru.citeck.ecos.records3.record.dao.AbstractRecordsDao
 import ru.citeck.ecos.records3.record.dao.query.RecordsQueryDao
 import ru.citeck.ecos.records3.record.dao.query.dto.query.RecordsQuery
 
 @Component
-class ActivityTypeRecordDao : AbstractRecordsDao(), RecordsQueryDao {
+class ActivityTypeRecordDao(
+    private val typeRefService: TypeRefService
+) : AbstractRecordsDao(), RecordsQueryDao {
 
     companion object {
         const val ID = "activity-type"
         private val ACTIVITY_TYPE_REF = ModelUtils.getTypeRef("ecos-activity")
-        private const val PARENTS_ATT = "parents"
     }
 
     override fun getId(): String {
@@ -23,19 +22,8 @@ class ActivityTypeRecordDao : AbstractRecordsDao(), RecordsQueryDao {
     }
 
     override fun queryRecords(recsQuery: RecordsQuery): Any? {
-        return AuthContext.runAsSystem {
-            recordsService.query(
-                RecordsQuery.create()
-                    .withSourceId(ACTIVITY_TYPE_REF.getSourceId())
-                    .withLanguage(PredicateService.LANGUAGE_PREDICATE)
-                    .withQuery(
-                        Predicates.contains(PARENTS_ATT, ACTIVITY_TYPE_REF.toString())
-                    )
-                    .withSortBy(recsQuery.sortBy)
-                    .withGroupBy(recsQuery.groupBy)
-                    .withPage(recsQuery.page)
-                    .build()
-            )
-        }
+        val result = typeRefService.expandWithChildren(ACTIVITY_TYPE_REF).toMutableList()
+        result.remove(ACTIVITY_TYPE_REF)
+        return result
     }
 }
