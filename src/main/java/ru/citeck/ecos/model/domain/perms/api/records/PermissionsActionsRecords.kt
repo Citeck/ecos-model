0296edd61070928e85d0070ebe303fec2421cbd7
Pavel Simonov <pavel.simonov@citeck.ru>
2024-10-03 11:54:17 +0700

    ECOSCOM-5572 - Fix PermissionsActionsRecords. Release 2.28.6

--- src/main/java/ru/citeck/ecos/model/domain/perms/api/records/PermissionsActionsRecords.kt
+++ src/main/java/ru/citeck/ecos/model/domain/perms/api/records/PermissionsActionsRecords.kt
@@ -7,10 +7,12 @@ import ru.citeck.ecos.context.lib.auth.AuthContext
 import ru.citeck.ecos.context.lib.auth.AuthRole
 import ru.citeck.ecos.data.sql.records.DbRecordsControlAtts
 import ru.citeck.ecos.records2.RecordConstants
+import ru.citeck.ecos.records2.predicate.model.Predicate
 import ru.citeck.ecos.records2.predicate.model.Predicates
-import ru.citeck.ecos.records3.iter.IterableRecords
+import ru.citeck.ecos.records3.iter.IterableRecordRefs
 import ru.citeck.ecos.records3.iter.IterableRecordsConfig
 import ru.citeck.ecos.records3.iter.PageStrategy
+import ru.citeck.ecos.records3.record.atts.dto.RecordAtts
 import ru.citeck.ecos.records3.record.dao.AbstractRecordsDao
 import ru.citeck.ecos.records3.record.dao.mutate.ValueMutateDao
 import ru.citeck.ecos.records3.record.dao.query.dto.query.RecordsQuery
@@ -25,6 +27,8 @@ class PermissionsActionsRecords : AbstractRecordsDao(), ValueMutateDao<Permissio
         const val ID = "update-permissions"
 
         private val log = KotlinLogging.logger {}
+
+        private const val BATCH_SIZE = 10
     }
 
     @Secured(AuthRole.ADMIN, AuthRole.SYSTEM)
@@ -36,29 +40,46 @@ class PermissionsActionsRecords : AbstractRecordsDao(), ValueMutateDao<Permissio
         } else {
             thread(start = true) {
                 log.info { "Start permissions update for type ${value.typeRef}" }
+                var processedCount = 0
                 AuthContext.runAsSystem {
-                    val records = IterableRecords(
+                    var query: Predicate = Predicates.eq(RecordConstants.ATT_TYPE, value.typeRef)
+                    if (value.predicate != Predicates.alwaysTrue()) {
+                        query = Predicates.and(query, value.predicate)
+                    }
+                    val records = IterableRecordRefs(
                         RecordsQuery.create()
                             .withEcosType(value.typeRef.getLocalId())
-                            .withQuery(Predicates.eq(RecordConstants.ATT_TYPE, value.typeRef))
+                            .withQuery(query)
                             .build(),
                         IterableRecordsConfig(100, PageStrategy.CREATED, emptyMap<String, String>()),
                         recordsService
                     ).iterator()
-                    var processedCount = 0
                     var reportCounter = 0
                     while (records.hasNext()) {
-                        TxnContext.doInNewTxn {
-                            recordsService.mutateAtt(records.next(), DbRecordsControlAtts.UPDATE_PERMISSIONS, true)
+                        val batch = ArrayList<EntityRef>()
+                        batch.add(records.next())
+                        while (batch.size < BATCH_SIZE && records.hasNext()) {
+                            batch.add(records.next())
                         }
-                        processedCount++
+                        TxnContext.doInNewTxn {
+                            val mutateAtts = batch.map {
+                                val atts = RecordAtts(it)
+                                atts[DbRecordsControlAtts.UPDATE_PERMISSIONS] = true
+                                atts
+                            }
+                            recordsService.mutate(mutateAtts)
+                        }
+                        processedCount += batch.size
                         if (processedCount / 1000 > reportCounter) {
                             reportCounter++
                             log.info { "Permissions updated for $processedCount records with type ${value.typeRef}" }
                         }
                     }
                 }
-                log.info { "Permissions updated for type ${value.typeRef}" }
+                log.info {
+                    "Permissions updating finished. " +
+                    "Processed $processedCount records with type ${value.typeRef}"
+                }
             }
         }
         log.info { "Permissions updating started for type ${value.typeRef}" }
@@ -71,6 +92,7 @@ class PermissionsActionsRecords : AbstractRecordsDao(), ValueMutateDao<Permissio
 
     class ActionDto(
         val recordRef: EntityRef = EntityRef.EMPTY,
-        val typeRef: EntityRef = EntityRef.EMPTY
+        val typeRef: EntityRef = EntityRef.EMPTY,
+        val predicate: Predicate = Predicates.alwaysTrue()
     )
 }
