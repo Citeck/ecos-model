b8eecb4ce6924c57dcb4a8209a0debf9ce4b8701
Pavel Simonov <pavel.simonov@citeck.ru>
2025-04-24 17:02:18 +0700

    ECOSCOM-5768 - Troubles with picture displaying. Release 2.30.3

--- src/main/java/ru/citeck/ecos/model/domain/comments/api/extractor/CommentExtractor.kt
+++ src/main/java/ru/citeck/ecos/model/domain/comments/api/extractor/CommentExtractor.kt
@@ -1,12 +1,45 @@
 package ru.citeck.ecos.model.domain.comments.api.extractor
 
+import org.jsoup.Jsoup
 import org.springframework.stereotype.Component
 import ru.citeck.ecos.commons.json.Json
 import ru.citeck.ecos.webapp.api.entity.EntityRef
 import ru.citeck.ecos.webapp.api.entity.toEntityRef
+import java.net.URLDecoder
+import java.nio.charset.StandardCharsets
 
 @Component
 class CommentExtractor {
+
+    companion object {
+        private const val REF_URL_PARAM_PREFIX = "ref="
+    }
+
+    fun extractAttachRefsFromText(text: String): Map<String, EntityRef> {
+
+        val result = LinkedHashMap<String, EntityRef>()
+
+        extractAttachmentsRefs(extractJsonStrings(text)).forEach {
+            result[it.toString()] = it
+        }
+
+        val fragment = Jsoup.parseBodyFragment(text)
+
+        val images = fragment.getElementsByTag("img")
+
+        for (image in images) {
+            val src = image.attribute("src").value
+            val urlArgs = src.substringAfter("?", "").split("&")
+            for (argument in urlArgs) {
+                if (argument.startsWith(REF_URL_PARAM_PREFIX)) {
+                    val refValue = argument.substring(REF_URL_PARAM_PREFIX.length)
+                    result[refValue] = EntityRef.valueOf(URLDecoder.decode(refValue, StandardCharsets.UTF_8))
+                }
+            }
+        }
+
+        return result
+    }
 
     fun extractJsonStrings(text: String?): List<String> {
         if (text.isNullOrBlank()) {
@@ -14,7 +47,7 @@ class CommentExtractor {
         }
         val jsonStrings = mutableListOf<String>()
 
-        text?.let {
+        text.let {
             val pattern = Regex("\\{.*?\\}")
             val matcher = pattern.findAll(text)
             jsonStrings.addAll(matcher.map { it.value })
