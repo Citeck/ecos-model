dd46cd86a6fb6e8024c155fa2fe872a1bd3c6fd1
Daniil Shcherbina <daniil.shcherbina@citeck.ru>
2025-07-16 17:36:13 +0300

    ECOSCRM-99 - Statistics for Citeck CRM

--- src/main/java/ru/citeck/ecos/model/domain/activity/api/records/ActivityTypeRecordDao.kt
+++ src/main/java/ru/citeck/ecos/model/domain/activity/api/records/ActivityTypeRecordDao.kt
@@ -3,9 +3,11 @@ package ru.citeck.ecos.model.domain.activity.api.records
 import org.springframework.stereotype.Component
 import ru.citeck.ecos.model.lib.type.service.TypeRefService
 import ru.citeck.ecos.model.lib.utils.ModelUtils
+import ru.citeck.ecos.records3.record.atts.schema.annotation.AttName
 import ru.citeck.ecos.records3.record.dao.AbstractRecordsDao
 import ru.citeck.ecos.records3.record.dao.query.RecordsQueryDao
 import ru.citeck.ecos.records3.record.dao.query.dto.query.RecordsQuery
+import ru.citeck.ecos.webapp.api.entity.EntityRef
 
 @Component
 class ActivityTypeRecordDao(
@@ -15,6 +17,11 @@ class ActivityTypeRecordDao(
     companion object {
         const val ID = "activity-type"
         private val ACTIVITY_TYPE_REF = ModelUtils.getTypeRef("ecos-activity")
+        private val PLANNED_ACTIVITY_TYPE_REF = ModelUtils.getTypeRef("planned-activity")
+        private val IMMEDIATE_ACTIVITY_TYPE_REF = ModelUtils.getTypeRef("immediate-activity")
+
+        private val SYSTEM_ACTIVITIES_TYPE_REFS =
+            listOf(ACTIVITY_TYPE_REF, PLANNED_ACTIVITY_TYPE_REF, IMMEDIATE_ACTIVITY_TYPE_REF)
     }
 
     override fun getId(): String {
@@ -22,8 +29,32 @@ class ActivityTypeRecordDao(
     }
 
     override fun queryRecords(recsQuery: RecordsQuery): Any? {
-        val result = typeRefService.expandWithChildren(ACTIVITY_TYPE_REF).toMutableList()
-        result.remove(ACTIVITY_TYPE_REF)
-        return result
+        val activityIds = typeRefService.expandWithChildren(ACTIVITY_TYPE_REF).toMutableList()
+        activityIds.removeAll(SYSTEM_ACTIVITIES_TYPE_REFS)
+
+        val activities = recordsService.getAtts(activityIds, ActivityDto::class.java)
+        val query = recsQuery.getQueryOrNull(QueryDto::class.java)
+        if (query?.typeRef != null && !query.typeRef.isEmpty()) {
+            val result = activities.filter { activity ->
+                val config = activity.config
+                config == null || config.availableTypes.isEmpty() || config.availableTypes.contains(query.typeRef)
+            }.map { it.id }
+            return result
+        }
+        return activityIds
     }
+
+    private data class QueryDto(
+        val typeRef: EntityRef? = null
+    )
+
+    private data class ActivityDto(
+        val id: EntityRef,
+        @AttName("aspectById.activity-config.config?json")
+        val config: ActivityConfig? = null
+    )
+
+    private data class ActivityConfig(
+        val availableTypes: List<EntityRef> = emptyList()
+    )
 }
