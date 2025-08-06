1be287af11240de93bc33e91a2301bddd7590433
Pavel Simonov <pavel.simonov@citeck.ru>
2025-08-06 14:09:24 +0700

    ECOSCOM-5697 - Add auth-role dao

--- src/main/java/ru/citeck/ecos/model/domain/authorities/api/records/AuthorityDao.kt
+++ src/main/java/ru/citeck/ecos/model/domain/authorities/api/records/AuthorityDao.kt
@@ -4,9 +4,7 @@ import org.springframework.stereotype.Component
 import ru.citeck.ecos.commons.data.MLText
 import ru.citeck.ecos.context.lib.auth.AuthRole
 import ru.citeck.ecos.context.lib.i18n.I18nContext
-import ru.citeck.ecos.model.domain.workspace.desc.WorkspaceMemberDesc
-import ru.citeck.ecos.model.lib.ModelServiceFactory
-import ru.citeck.ecos.model.lib.attributes.computed.ComputedAttsService
+import ru.citeck.ecos.model.domain.workspace.dto.WorkspaceMemberRole
 import ru.citeck.ecos.records2.predicate.model.Predicate
 import ru.citeck.ecos.records2.predicate.model.Predicates
 import ru.citeck.ecos.records3.record.atts.schema.annotation.AttName
@@ -14,15 +12,9 @@ import ru.citeck.ecos.records3.record.dao.AbstractRecordsDao
 import ru.citeck.ecos.records3.record.dao.atts.RecordAttsDao
 import ru.citeck.ecos.records3.record.dao.query.RecordsQueryDao
 import ru.citeck.ecos.records3.record.dao.query.dto.query.RecordsQuery
-import ru.citeck.ecos.webapp.api.entity.EntityRef
-import ru.citeck.ecos.webapp.lib.model.type.registry.EcosTypesRegistry
-import java.time.Instant
 
 @Component
-class AuthorityDao(
-    private val typesRegistry: EcosTypesRegistry,
-    modelServices: ModelServiceFactory
-) : RecordsQueryDao, RecordAttsDao, AbstractRecordsDao() {
+class AuthorityDao : RecordsQueryDao, RecordAttsDao, AbstractRecordsDao() {
 
     companion object {
         const val SRC_ID = "authority"
@@ -35,13 +27,22 @@ class AuthorityDao(
                 I18nContext.ENGLISH to "System Administrator"
             ),
             AuthRole.ADMIN
+        ),
+        RoleValue(
+            MLText(
+                I18nContext.RUSSIAN to "Участник рабочего пространства",
+                I18nContext.ENGLISH to "Workspace Member"
+            ),
+            AuthRole.PREFIX + "WS_" + WorkspaceMemberRole.USER
+        ),
+        RoleValue(
+            MLText(
+                I18nContext.RUSSIAN to "Менеджер рабочего пространства",
+                I18nContext.ENGLISH to "Workspace Manager"
+            ),
+            AuthRole.PREFIX + "WS_" + WorkspaceMemberRole.MANAGER
         )
     )
-
-    private var computedAttsService: ComputedAttsService = modelServices.computedAttsService
-
-    private var wsMemberTypeChangedAt = Instant.EPOCH
-    private var cachedWsRoles = emptyList<RoleValue>()
 
     override fun getRecordAtts(recordId: String): Any? {
         if (recordId.startsWith(AuthRole.PREFIX)) {
@@ -66,26 +67,7 @@ class AuthorityDao(
     }
 
     private fun getRoles(): List<RoleValue> {
-
-        val rolesRes = ArrayList<RoleValue>(defaultRoles)
-
-        val memberType = typesRegistry.getValueWithMeta(WorkspaceMemberDesc.TYPE_ID)
-
-        if (memberType != null && wsMemberTypeChangedAt.isBefore(memberType.meta.modified)) {
-
-            val memberRoleAtt = memberType.entity.model.attributes
-                .find { it.id == WorkspaceMemberDesc.ATT_MEMBER_ROLE }
-
-            if (memberRoleAtt != null) {
-                cachedWsRoles = computedAttsService.getAttOptions(EntityRef.EMPTY, memberRoleAtt.config).map {
-                    RoleValue(it.label, "ROLE_WS_" + it.value)
-                }
-            }
-            wsMemberTypeChangedAt = memberType.meta.modified
-        }
-        rolesRes.addAll(cachedWsRoles)
-
-        return rolesRes
+        return defaultRoles
     }
 
     override fun getId(): String {
