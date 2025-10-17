5b63670784745a2c3e89f8d5c6eb82648a1f973a
Pavel Simonov <pavel.simonov@citeck.ru>
2025-10-17 14:10:51 +0700

    ECOSCOM-5697 - Add ability to create artifacts within workspace

---
+++ src/test/java/ru/citeck/ecos/model/domain/workspace/WorkspaceSystemIdUtilsTest.kt
@@ -0,0 +1,24 @@
+package ru.citeck.ecos.model.domain.workspace
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
+import ru.citeck.ecos.model.domain.workspace.utils.WorkspaceSystemIdUtils
+
+class WorkspaceSystemIdUtilsTest {
+
+    @ParameterizedTest
+    @CsvSource(
+        "1, 1",
+        "Q..........................q_, Q_q",
+        "abc_____def, abc_def",
+        "$$$, RND"
+    )
+    fun test(input: String, output: String) {
+        if (output == "RND") {
+            assertThat(WorkspaceSystemIdUtils.createId(input) { false }.matches("[a-z234567]{4,}".toRegex())).isTrue()
+        } else {
+            assertThat(WorkspaceSystemIdUtils.createId(input) { false }).isEqualTo(output)
+        }
+    }
+}
